72e53db4f561fd24c0fbc03facf0518c0efd40e9
absencee_ <84275165+b1lodHand@users.noreply.github.com>
2024-03-31 12:42:26 +0300

    initial commit

---
+++ Editor/HierarchyIconDisplay.cs
@@ -0,0 +1,60 @@
+using com.absence.editor.helpers;
+using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace com.absence.editor
+{
+    [InitializeOnLoad]
+    public static class HierarchyIconDisplay
+    {
+        static bool m_hierarchyHasFocus = false;
+        static EditorWindow m_hierarchyEditorWindow;
+
+        static HierarchyIconDisplay()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindowItemGUI;
+            EditorApplication.update += OnEditorUpdate;
+        }
+
+        private static void OnEditorUpdate()
+        {
+            if (m_hierarchyEditorWindow == null)
+                m_hierarchyEditorWindow = EditorWindow.GetWindow(System.Type.GetType("UnityEditor.SceneHierarchyWindow,UnityEditor"));
+
+            m_hierarchyHasFocus = EditorWindow.focusedWindow != null && EditorWindow.focusedWindow == m_hierarchyEditorWindow;
+        }
+
+        private static void OnHierarchyWindowItemGUI(int instanceID, Rect selectionRect)
+        {
+            GameObject obj = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+            if (obj == null) return;
+
+            Component[] components = obj.GetComponents<Component>();
+            if (components == null || components.Length == 0) return;
+
+            Component component = components.Length > 1 ? components[1] : components[0];
+
+            Type type = component.GetType();
+
+            GUIContent content = EditorGUIUtility.ObjectContent(component, type);
+            content.text = null;
+            content.tooltip = type.Name;
+
+            if (content.image == null) return;
+
+            bool isSelected = Selection.instanceIDs.Contains(instanceID);
+            bool isHovering = selectionRect.Contains(Event.current.mousePosition);
+
+
+            Color color = UnityEditorBackgroundColor.Get(isSelected, isHovering, m_hierarchyHasFocus);
+            Rect backgroundRect = selectionRect;
+            backgroundRect.width = 18.5f;
+            EditorGUI.DrawRect(backgroundRect, color);
+
+            EditorGUI.LabelField(selectionRect, content);
+        }
+    }
+
+}
