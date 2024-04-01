480cd33d1e4ad56bab01df2d0bb8a32214aaf523
absencee_ <84275165+b1lodHand@users.noreply.github.com>
2024-04-01 11:46:07 +0300

    added FolderOpenHandler
    lets you reveal the selected folder in explorer when you control + double click it. (works only in one column layout)

---
+++ Editor/FolderOpenHandler.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace com.absence.editor
+{
+    public class FolderOpenHandler
+    {
+        [OnOpenAsset]
+        public static bool OnOpenAsset(int instanceId)
+        {
+            Event e = Event.current;
+            if (e == null || !e.control)
+                return false;
+
+            Object obj = EditorUtility.InstanceIDToObject(instanceId);
+            string path = AssetDatabase.GetAssetPath(obj);
+            if (AssetDatabase.IsValidFolder(path))
+            {
+                EditorUtility.RevealInFinder(path);
+            }
+
+            return true;
+        }
+    }
+
+}
